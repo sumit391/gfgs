931d9edc362338fdfc3b9d1821a60a002bfb1b0d
sumit391 <sumitatbits@gmail.com>
2016-02-23 05:39:49 +0100

    jobbseq

---
+++ dynProg/longIncreSubseq.cpp
@@ -0,0 +1,23 @@
+// Author : Sumit Chouhan
+// ETH Zürich
+// Date : 17/02/2016
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include "../searchAndSort/mergeSort.h"
+
+using namespace std;
+
+
+int main(int argc, char **argv){
+	int count;cin >> count;
+	vector<int>	num(count);
+	for (int i=0;i<count;i++){
+		cin >> num[i];
+	}
+
+	return 0;
+}
+
